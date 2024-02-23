b9882dbd5dd879e068712a3f92c92eb8d6ac781f
Andreas2298 <andre-as13@hotmail.no>
2024-02-23 15:39:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using HTTPUtils;
+using System.Text.Json;
+using AnsiTools;
+using Colors = AnsiTools.ANSICodes.Colors;
+
+Console.Clear();
+Console.WriteLine("Starting Assignment 2");
+
+// SETUP 
+const string myPersonalID = "72813527b56b46ba1fecfe21f627c020cf087f59ec1ef5d192af752a269e8a92";
+const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
+const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+
+// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
+HttpUtils httpUtils = HttpUtils.instance;
+
+//#### REGISTRATION
+// We start by registering and getting the first task
+Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
+string taskID = "psu31_4"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+//#### FIRST TASK 
+// Fetch the details of the task from the server.
+Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task1?.title}{ANSICodes.Reset}\n{task1?.description}\nParameters: {Colors.Yellow}{task1?.parameters}{ANSICodes.Reset}");
+
+// Calculate the answer to the task
+var answer = task1?.parameters.Split(",").Select(p => int.Parse(p.Trim())).Aggregate<int, int>(0, (acc, n) => acc + n);
+Console.WriteLine(answer);
+
+
+// Send the answer to the server
+Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer.ToString());
+Console.WriteLine($"Answer: {Colors.Green}{task1AnswerResponse}{ANSICodes.Reset}");
+
+taskID = "aAaa23"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+Console.WriteLine("\n-----------------------------------\n");
+
+//#### SECOND TASK
+// Fetch the details of the task from the server.
+
+Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task2 = JsonSerializer.Deserialize<Task>(task2Response.content);
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task2?.title}{ANSICodes.Reset}\n{task2?.description}\nParameters: {Colors.Yellow}{task2?.parameters}{ANSICodes.Reset}");
+
+
+// Convert from fahrenheit to celcius 
+
+
+string tempF = task2.parameters;
+
+Console.WriteLine(tempF);
+
+float tempFValue = float.Parse(tempF);
+float tempC = (tempFValue - 32) * 5 / 9;
+float tempCR = (float)Math.Round(tempC, 2);
+
+string tempCRS = tempCR.ToString().Replace(",", ".");
+Console.WriteLine(tempCR);
+
+
+
+// Send the answer to the server
+Response task2AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, tempCRS);
+Console.WriteLine($"\nAnswer2: {Colors.Green}{task2AnswerResponse}{ANSICodes.Reset}");
+
+taskID = "rEu25ZX";
+
+Console.WriteLine("\n-----------------------------------\n");
+
+//#### Third TASK
+// Fetch the details of the task from the server.
+
+Response task3Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task3 = JsonSerializer.Deserialize<Task>(task3Response.content);
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task3?.title}{ANSICodes.Reset}\n{task3?.description}\nParameters: {Colors.Yellow}{task3?.parameters}{ANSICodes.Reset}");
+
+string roman = task3.parameters;
+
+Console.WriteLine("");
+
+// Return interger value of the numeral
+
+
+
+
+
+
+
+
+
+class Task
+{
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? taskID { get; set; }
+    public string? usierID { get; set; }
+    public string? parameters { get; set; }
+
+}
+
